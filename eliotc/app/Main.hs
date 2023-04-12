384699a578b1bc07c62f53dc1fe014737ae3bcbc
Robert Brautigam <robert.brautigam@prewave.ai>
2023-04-12 21:55:18 +0200

    Parse filepaths

--- eliotc/app/Main.hs
+++ eliotc/app/Main.hs
@@ -2,18 +2,22 @@ module Main (main) where
 
 import System.Console.CmdArgs.Explicit
 
+data CommandLineParameters = CommandLineParameters { help :: Bool, sourceDirs :: [String] }
+   deriving Show
+
 -- | Start the compiler by parsing arguments and calling the
 -- compile function with the appropriate parameters.
 main :: IO ()
 main = do
    args <- processArgs arguments
-   if ("help", "") `elem` args then
+   if help args then
       print $ helpText [] HelpFormatDefault arguments
    else
       print args
 
-arguments :: Mode [(String, String)]
-arguments = mode "eliotc" [] "The ELIOT Programming Language compiler."
-   (flagArg (save "source") "[SOURCEDIR]...")
-   [flagHelpSimple (("help", ""):)]
-   where save key value acc = Right $ (key, value):acc
+-- | Define the command line arguments the compile can accept.
+arguments :: Mode CommandLineParameters
+arguments = mode "eliotc" (CommandLineParameters False []) "The ELIOT Programming Language compiler."
+   (flagArg addPath "[SOURCEDIR]...")
+   [flagHelpSimple (\c -> c {help = True})]
+   where addPath pathCandidate commandLineParameters = Right $ commandLineParameters { sourceDirs = pathCandidate: sourceDirs commandLineParameters }
