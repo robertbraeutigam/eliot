f1c824ba2794721274109f5217aa107f804c4d27
Robert Brautigam <robert.brautigam@prewave.ai>
2023-04-12 21:18:45 +0200

    Add rudimentary command line parsing

--- eliotc/app/Main.hs
+++ eliotc/app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
-import Lib
+import System.Console.CmdArgs.Explicit
 
+-- | Start the compiler by parsing arguments and calling the
+-- compile function with the appropriate parameters.
 main :: IO ()
-main = someFunc
+main = do
+   args <- processArgs arguments
+   if ("help", "") `elem` args then
+      print $ helpText [] HelpFormatDefault arguments
+   else
+      print args
+
+arguments :: Mode [(String, String)]
+arguments = mode "eliotc" [] "The ELIOT Programming Language compiler."
+   (flagArg (save "source") "[SOURCEDIR]...")
+   [flagHelpSimple (("help", ""):)]
+   where save key value acc = Right $ (key, value):acc
