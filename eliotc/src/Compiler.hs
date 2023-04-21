402392a3ced6ec8e00355f96a4ffdb0f7a4d734b
Robert Brautigam <robert.brautigam@prewave.ai>
2023-04-21 11:37:43 +0200

    Refactor processors themselves to be in compiler

--- eliotc/src/Compiler.hs
+++ eliotc/src/Compiler.hs
@@ -1,14 +1,36 @@
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric #-}
 {-| Defines the compiler for the ELIOT language. The compiler
  - takes a set of source paths, and produces a single output program.
  -}
 
 module Compiler(compile) where
 
+import Control.Monad
+import Control.Monad.Trans.Class
+import Data.List (isPrefixOf, isSuffixOf)
+import System.FilePath
+import System.Directory
+import GHC.Generics
+import Data.Hashable
 import FactEngine
-import CompilerFacts
 import Logging
-import Files
-import TokenParser
+import Tokens
+
+-- These data types define all the various stages of the compilation process
+
+data Signal =
+     SourcePathDetected FilePath
+   | SourceFileDetected FilePath
+   | SourceFileRead     FilePath
+   deriving (Eq, Show, Generic, Hashable)
+
+data Fact = 
+     SourcePath FilePath                -- A path to some file or directory containing source code
+   | SourceFile FilePath                -- A source file that has been detected
+   | SourceFileContent String           -- Contents of a source file
+   deriving (Eq, Show)
+
+type CompilerProcessor = FactProcessor Signal Fact
 
 -- | Run the compiler on the given source paths.
 compile :: [String] -> IO ()
@@ -20,3 +42,25 @@ compile paths = do
       Nothing        -> errorMsg "Compiler terminated with errors. See previous errors for details."
    where sourcePathFacts = map (\s -> (SourcePathDetected s, SourcePath s)) paths
          processors = [directoryWalker, fileReader, parseTokensProcessor]
+
+-- From here on are the processors for the compilation process
+
+directoryWalker :: CompilerProcessor
+directoryWalker (SourcePath path) = do
+   isFile      <- lift $ doesFileExist path
+   isDirectory <- lift $ doesDirectoryExist path
+   when isFile       $ registerFact (SourceFileDetected path) (SourceFile path) 
+   when isDirectory  $ (lift $ filter (not . isPrefixOf ".") <$> listDirectory path) >>= mapM_ ((registerFact . SourcePathDetected <*> SourcePath) . (path </>))
+   when ((not isFile) && (not isDirectory)) $ errorMsg $ "Path " ++ path ++ " is neither a file nor directory"
+directoryWalker _ = return ()
+
+fileReader :: CompilerProcessor
+fileReader (SourceFile path)
+   | ".els" `isSuffixOf` path = (lift $ readFile path) >>= (registerFact (SourceFileRead path) . SourceFileContent)
+   | otherwise                = debugMsg $ "Ignoring source file because not ending in '.els': " ++ path
+fileReader _ = return ()
+
+parseTokensProcessor :: CompilerProcessor
+parseTokensProcessor (SourceFileContent code) = debugMsg $ show $ parseTokens code
+parseTokensProcessor _ = return ()
+
--- eliotc/src/Tokens.hs
+++ eliotc/src/Tokens.hs
@@ -1,10 +1,38 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
 {-| Tokens of the ELIOT language. Tokens are the next building block above characters the language
  - is made out of.
  -}
 
-module Tokens (Token(..)) where
+module Tokens (Token(..), parseTokens) where
+
+import Text.Parsec
+import Data.Char (isSpace)
+import Control.Monad
 
 data Token = Identifier String   -- Satisfies the rules for a generic identifier ~alphanumeric
            | Symbol String       -- Sort-of identifier comprised of non-alphanumberic characters
    deriving (Show)
+
+parseTokens :: String -> Either ParseError [Token]
+parseTokens code = parse (whiteSpace >> (many ((identifier <|> operator) <* whiteSpace)) <* eof) "" code
+
+identifier = do
+   firstCharacter <- letter
+   restCharacters <- many (alphaNum <|> oneOf "_'")
+   return $ Identifier (firstCharacter:restCharacters)
+
+operator = (many1 $ oneOf ":!#$%&*+./<=>?@\\^|-~") >>= (return . Symbol)
+
+-- | Whitespace includes everything from spaces, newlines to comments.
+whiteSpace = skipMany $ simpleSpace <|> oneLineComment <|> multiLineComment
+
+simpleSpace = skipMany1 (satisfy isSpace)
+
+oneLineComment = try (string "//") >> skipMany (satisfy (/= '\n'))
+
+multiLineComment = try (string "/*") >> multiLineCommentBody
+
+multiLineCommentBody = (void $ try (string "*/"))                             -- Comment ended
+                   <|> (skipMany1 (satisfy (/= '*')) >> multiLineCommentBody) -- Skip to next *
+                   <|> ((void $ char '*') >> multiLineCommentBody)            -- Skip * if string "*/" didn't match
 
