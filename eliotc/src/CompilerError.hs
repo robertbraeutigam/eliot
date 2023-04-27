5f7449294d0376f4efe890618249967a99d1f088
Robert Brautigam <robert.brautigam@prewave.ai>
2023-04-27 21:32:59 +0200

    Fix tests

--- eliotc/src/CompilerError.hs
+++ eliotc/src/CompilerError.hs
@@ -6,6 +6,8 @@ module CompilerError (SourcePosition(..), CompilerError(..)) where
 import Text.Parsec
 
 data SourcePosition = SourcePosition { row::Line, col::Column }
+   deriving (Show, Eq)
 
 data CompilerError = CompilerError { errorFrom::SourcePosition, errorTo::SourcePosition, errorMessage::String }
+   deriving (Show, Eq)
 
--- eliotc/test/TokensSpec.hs
+++ eliotc/test/TokensSpec.hs
@@ -2,35 +2,40 @@ module TokensSpec (spec) where
 
 import Data.Either
 import Test.Hspec
+import Data.Either.Extra
 import Tokens
+import CompilerError
 
 spec :: Spec
 spec = do
    describe "parsing tokens" $ do
       it "should not return tokens in line comments" $ do
-         parseTokens "// not tokens" `shouldBe` Right []
+         parseJustTokens "// not tokens" `shouldBe` Right []
 
       it "should return tokens before line comments" $ do
-         parseTokens "token // not tokens" `shouldBe` Right [Identifier "token"]
+         parseJustTokens "token // not tokens" `shouldBe` Right [Identifier "token"]
 
       it "should return tokens after line comments in the next line" $ do
-         parseTokens "// not tokens\nagain" `shouldBe` Right [Identifier "again"]
+         parseJustTokens "// not tokens\nagain" `shouldBe` Right [Identifier "again"]
 
       it "should not return tokens in block comments" $ do
-         parseTokens "/* not tokens */" `shouldBe` Right []
+         parseJustTokens "/* not tokens */" `shouldBe` Right []
 
       it "should not return tokens in block comments with multiple lines" $ do
-         parseTokens "/* not tokens\nagain no\ntokens here */" `shouldBe` Right []
+         parseJustTokens "/* not tokens\nagain no\ntokens here */" `shouldBe` Right []
 
       it "should return tokens before block comments" $ do
-         parseTokens "token /* not tokens */" `shouldBe` Right [Identifier "token"]
+         parseJustTokens "token /* not tokens */" `shouldBe` Right [Identifier "token"]
 
       it "should return tokens after block comments" $ do
-         parseTokens "/* not tokens */again" `shouldBe` Right [Identifier "again"]
+         parseJustTokens "/* not tokens */again" `shouldBe` Right [Identifier "again"]
 
       it "should fail on broken block comment" $ do
-         isLeft (parseTokens "/* block comment not balanced") `shouldBe` True
+         isLeft (parseJustTokens "/* block comment not balanced") `shouldBe` True
 
       it "should not parse illegal tokens" $ do
-         isLeft (parseTokens "→") `shouldBe` True
+         isLeft (parseJustTokens "→") `shouldBe` True
+
+parseJustTokens :: String -> Either CompilerError [Token]
+parseJustTokens str = mapRight (map positionedToken) (parseTokens str)
 
