2bc60444fed8a4f7c277a022e4c797989c30a2e1
Robert Brautigam <robert.brautigam@prewave.ai>
2023-04-28 10:20:58 +0200

    Add some more tests to AST

--- eliotc/test/ASTSpec.hs
+++ eliotc/test/ASTSpec.hs
@@ -15,6 +15,16 @@ spec = do
       it "should parse multiple correct import statements" $ do
          parseCode "import a.b.C\nimport b.c.D\n\nimport e.f.h.G" `shouldBe` importAST [Import ["a", "b"] "C", Import ["b", "c"] "D", Import ["e", "f", "h"] "G"]
 
+      it "should not parse import with non-capitalized module name" $ do
+         countASTErrors (parseCode "import a.b.c") `shouldBe` 1
+
+      it "should report multiple errors" $ do
+         countASTErrors (parseCode "import a.b.c\nimport d;e\n") `shouldSatisfy` (>=2)
+
+countASTErrors result = case result of
+   Left _            -> 0
+   Right (errs, _)   -> length errs
+
 importAST imps = Right ([], AST imps)
 
 parseCode code = mapRight parseAST (parseTokens code)
