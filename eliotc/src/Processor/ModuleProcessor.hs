55b14dfdd67896562387c44a25ee66ecfc791470
Robert Brautigam <robert.brautigam@prewave.ai>
2023-11-09 15:45:49 +0100

    Add more debug

--- eliotc/src/Processor/ModuleProcessor.hs
+++ eliotc/src/Processor/ModuleProcessor.hs
@@ -23,7 +23,7 @@ parseModuleProcessor (SourceAST path ast) = do
          functionNames     <- sequence $ map extractFunctionName (functionDefinitions ast)
          _                 <- registerCompilerFact (ModuleFunctionNamesRead mn) (ModuleFunctionNames mn (catMaybes functionNames))
          importedFunctions <- collectImportedFunctions (importStatements ast) Map.empty
-         debugMsg $ "Imported functions: " ++ (show importedFunctions)
+         debugMsg $ (show mn) ++ " imported functions: " ++ (show importedFunctions)
       Nothing -> compileOk
 parseModuleProcessor _ = compileOk
 
