4aad16db879ab566828bb21bcb5c5ec3ec923d5d
Robert Brautigam <robert.brautigam@prewave.ai>
2023-11-10 22:22:27 +0100

    Add tests for FAST

---
+++ eliotc/test/Processor/FASTProcessorSpec.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+module Processor.FASTProcessorSpec (spec) where
+
+import Test.Hspec
+import Processor.TokensProcessor
+import Processor.ASTProcessor
+import Processor.ModuleProcessor
+import Processor.FASTProcessor
+import Processor.TestCompiler
+import CompilerProcessor
+import FAST
+import Module
+
+spec :: Spec
+spec = do
+   describe "parsing fast" $ do
+      it "should result in a function application when parsing a function call" $ do
+         parseForFunction "ni = nu\nnu = a" "ni" `shouldReturn` (FunctionApplication $ FunctionFQN (ModuleName [] "A") "nu")
+
+      it "should indicate error when referred function is not defined" $ do
+         parseForErrors "ni = nu" `shouldReturn` ["Called function not defined."]
+         
+parseForErrors :: String -> IO [String]
+parseForErrors code = compileCollectFacts [parseTokensProcessor, parseASTProcessor, parseModuleProcessor, parseFASTProcessor] [("A", code)] selectErrors
+   where selectErrors (_, CompilerErrorFact (CompilerError _ _ _ msg)) = Just msg
+         selectErrors _                                                = Nothing
+
+
+parseForFunction :: String -> String -> IO FunctionBody
+parseForFunction code func = compileSelectFact [parseTokensProcessor, parseASTProcessor, parseModuleProcessor, parseFASTProcessor] [("A", code)] selectFact
+   where selectFact (_, CompiledFunction (FunctionFQN _ f) body) | f == func = Just body
+         selectFact _                                                        = Nothing
+
