55fe7c50535331076b9e99119ebea79c89fefd3e
Robert Brautigam <robert.brautigam@prewave.ai>
2023-11-24 22:06:16 +0100

    Add better show to module and function names

--- eliotc/src/Module.hs
+++ eliotc/src/Module.hs
@@ -7,19 +7,26 @@ module Module (ModuleName(..), FunctionFQN(..), FunctionDictionary) where
 
 import Data.Hashable
 import Data.Map
+import Data.List(intercalate)
 
 -- | A fully qualified module name.
 data ModuleName = ModuleName [String] String
- deriving (Eq, Show)
+   deriving (Eq)
+
+instance Show ModuleName where
+   show (ModuleName moduleNames name) = intercalate "." (moduleNames ++ [name])
 
 instance Hashable ModuleName where
-  hashWithSalt salt (ModuleName ps m) = hashWithSalt salt (ps, m)
+   hashWithSalt salt (ModuleName ps m) = hashWithSalt salt (ps, m)
 
 -- | Fully qualified function name.
 data FunctionFQN = FunctionFQN ModuleName String
- deriving (Eq, Show)
+   deriving (Eq)
+
+instance Show FunctionFQN where
+   show (FunctionFQN mn fn) = (show mn) ++ "." ++ fn
 
 instance Hashable FunctionFQN where
-  hashWithSalt salt (FunctionFQN mn n) = hashWithSalt salt (mn, n)
+   hashWithSalt salt (FunctionFQN mn n) = hashWithSalt salt (mn, n)
 
 type FunctionDictionary = Map String FunctionFQN
