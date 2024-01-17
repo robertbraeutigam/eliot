083dfee8c43a6884869345ec575e4da006d8459f
Robert Brautigam <robert.brautigam@prewave.ai>
2024-01-17 19:59:02 +0100

    Add STMVarTest

---
+++ eliotc/test/src/com/vanillasource/stm/STMVarTest.scala
@@ -0,0 +1,42 @@
+package com.vanillasource.stm
+
+import cats.effect.IO
+import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.syntax.all.*
+import com.vanillasource.stm.STM.*
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class STMVarTest extends AsyncFlatSpec with AsyncIOSpec with Matchers {
+  "var" should "return initial value" in {
+    val program = for {
+      stmRuntime <- createRuntime()
+      v          <- createSTMVar(100).commit(using stmRuntime)
+      result     <- v.get().commit(using stmRuntime)
+    } yield result
+
+    program.asserting(_ shouldBe 100)
+  }
+
+  it should "return set value" in {
+    val program = for {
+      stmRuntime <- createRuntime()
+      v          <- createSTMVar(100).commit(using stmRuntime)
+      _          <- v.set(999).commit(using stmRuntime)
+      result     <- v.get().commit(using stmRuntime)
+    } yield result
+
+    program.asserting(_ shouldBe 999)
+  }
+
+  it should "atomically update" in {
+    val program = for {
+      stmRuntime <- createRuntime()
+      v          <- createSTMVar(100).commit(using stmRuntime)
+      _          <- v.update(_ + 1).commit(using stmRuntime).parReplicateA_(900)
+      result     <- v.get().commit(using stmRuntime)
+    } yield result
+
+    program.asserting(_ shouldBe 1000)
+  }
+}
