de6a337c2060bae8a80070ea8c9e2a369e52596d
Robert Brautigam <robert.brautigam@prewave.ai>
2024-02-15 21:52:12 +0100

    Add cleaner debug

--- eliotc/src/com/vanillasource/eliot/eliotc/token/Tokenizer.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/token/Tokenizer.scala
@@ -3,18 +3,9 @@ package com.vanillasource.eliot.eliotc.token
 import cats.effect.IO
 import cats.syntax.all.*
 import com.vanillasource.eliot.eliotc.feedback.{Logging, User}
-import com.vanillasource.eliot.eliotc.source.{Position, PositionRange, SourceContent, Sourced, SourcedError}
+import com.vanillasource.eliot.eliotc.source.{SourceContent, SourcedError}
 import com.vanillasource.eliot.eliotc.{CompilationProcess, CompilerFact, CompilerProcessor}
-import parsley.Parsley
-import parsley.token.Lexer
-import parsley.token.descriptions.numeric.NumericDesc
-import parsley.token.descriptions.text.TextDesc
-import parsley.token.descriptions.{LexicalDesc, NameDesc, SpaceDesc, SymbolDesc, numeric, text}
-import parsley.token.predicate.Basic
-import parsley.position.pos
-import parsley.character
-import parsley.errors.combinator.markAsToken
-import parsley.errors.combinator.*
+import parsley.token.descriptions.{numeric, text}
 
 import java.io.File
 
@@ -32,7 +23,10 @@ class Tokenizer extends CompilerProcessor with Logging with User {
       .parse(content)(using new TokenErrorBuilder(file))
       .fold(
         errorMessage => SourcedError.compilerError(errorMessage),
-        tokens => debug(s"tokenized $file into $tokens") >> process.registerFact(SourceTokens(file, tokens))
+        tokens =>
+          debug(s"tokenized $file into: ${tokens.map(_.show).mkString(", ")}") >> process.registerFact(
+            SourceTokens(file, tokens)
+          )
       )
 
 }
