370ed78993c27e99cf056ccd5012b0675bf2f264
Robert Brautigam <robert.brautigam@prewave.ai>
2024-12-29 20:22:19 +0100

    Refactor

--- eliotc/src/com/vanillasource/eliot/eliotc/typesystem/TypeCheckProcessor.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/typesystem/TypeCheckProcessor.scala
@@ -30,71 +30,54 @@ class TypeCheckProcessor extends CompilerProcessor with Logging {
       body: Tree[Expression]
   )(using process: CompilationProcess): OptionT[IO, Unit] = {
     for {
-      topType <- checkCallTypes(body).toOptionT
+      topType <- checkCallTypes(body)
       _       <- if (topType.value === typeDefinition.typeName.value) {
                    process.registerFact(TypeCheckedFunction(ffqn, functionDefinition)).liftOptionT
                  } else {
                    compilerError(
                      topType.as(
-                       s"Return type is ${topType.value}, but function declared to return ${typeDefinition.typeName.value}"
+                       s"Expression type is ${topType.value}, but function declared to return ${typeDefinition.typeName.value}"
                      )
                    ).liftOptionT
                  }
     } yield ()
   }
 
-  /** @return
-    *   True, iff check all checks complete and no problems found.
-    */
-  private def checkCallTypes(body: Tree[Expression])(using CompilationProcess): IO[Option[Sourced[String]]] = body match
-    case Tree.Empty()                                       => None.pure
-    case Tree.Node(FunctionApplication(sourcedFfqn), nodes) =>
-      for {
-        recursiveResults <- nodes.map(checkCallTypes).sequence
-        result           <- checkCallType(sourcedFfqn, recursiveResults)
-      } yield result.map(sourcedFfqn.as(_))
-    case Tree.Node(IntegerLiteral(value), _)                => Some(value.as("Byte")).pure // TODO: Hardcoded for now
+  // TODO: just return case class with type name and whether there was an error!
+  private def checkCallTypes(expression: Tree[Expression])(using CompilationProcess): OptionT[IO, Sourced[String]] =
+    expression match
+      case Tree.Empty()                                       => OptionT.none
+      case Tree.Node(FunctionApplication(sourcedFfqn), nodes) =>
+        for {
+          argumentTypes <- nodes.map(checkCallTypes).sequence
+          returnType    <- checkCallType(sourcedFfqn, argumentTypes)
+        } yield sourcedFfqn.as(returnType)
+      case Tree.Node(IntegerLiteral(value), _)                => OptionT.pure(value.as("Byte")) // TODO: Hardcoded for now
 
-  /** @return
-    *   True, iff check is complete and no problems found.
-    */
-  private def checkCallType(sourcedFfqn: Sourced[FunctionFQN], calculatedArgumentTypes: Seq[Option[Sourced[String]]])(
-      using process: CompilationProcess
-  ): IO[Option[String]] = for {
-    functionDefinitionMaybe <- process.getFact(ResolvedFunction.Key(sourcedFfqn.value))
-    result                  <- functionDefinitionMaybe match
-                                 case Some(functionDefinition) =>
-                                   for {
-                                     _ <- if (calculatedArgumentTypes.length =!= functionDefinition.definition.arguments.length) {
-                                            compilerError(
-                                              sourcedFfqn.as(
-                                                s"Function is called with ${calculatedArgumentTypes.length} parameters, but needs ${functionDefinition.definition.arguments.length}."
-                                              )
-                                            )
-                                          } else {
-                                            // Check argument types one by one
-                                            calculatedArgumentTypes
-                                              .zip(functionDefinition.definition.arguments)
-                                              .map { (calculatedTypeMaybe, argumentDefinition) =>
-                                                calculatedTypeMaybe match
-                                                  case Some(calculatedType) =>
-                                                    compilerError(
-                                                      calculatedType.as(
-                                                        s"Expression had type ${calculatedType.value}, but needed: ${argumentDefinition.typeDefinition.typeName.value}"
-                                                      )
-                                                    ).whenA(calculatedType.value =!= argumentDefinition.typeDefinition.typeName.value)
-                                                  case None                 => IO.unit // No calculated type, so error is there
-                                              }
-                                              .sequence_
-                                          }
-                                   } yield Some(functionDefinition.definition.typeDefinition.typeName.value)
-                                 case None                     =>
-                                   // This should not happen, since we're after the resolve step
-                                   compilerError(
-                                     sourcedFfqn.as(
-                                       s"Could not resolve function."
-                                     )
-                                   ).as(None)
-  } yield result
-
+  // TODO: issue error if call was not resolved! could be a random function that is not defined
+  private def checkCallType(sourcedFfqn: Sourced[FunctionFQN], calculatedArgumentTypes: Seq[Sourced[String]])(using
+      process: CompilationProcess
+  ): OptionT[IO, String] = for {
+    functionDefinition <- process.getFact(ResolvedFunction.Key(sourcedFfqn.value)).toOptionT
+    _                  <- if (calculatedArgumentTypes.length =!= functionDefinition.definition.arguments.length) {
+                            compilerError(
+                              sourcedFfqn.as(
+                                s"Function is called with ${calculatedArgumentTypes.length} parameters, but needs ${functionDefinition.definition.arguments.length}."
+                              )
+                            ).liftOptionT
+                          } else {
+                            // Check argument types one by one
+                            calculatedArgumentTypes
+                              .zip(functionDefinition.definition.arguments)
+                              .map { (calculatedType, argumentDefinition) =>
+                                compilerError(
+                                  calculatedType.as(
+                                    s"Expression had type ${calculatedType.value}, but needed: ${argumentDefinition.typeDefinition.typeName.value}"
+                                  )
+                                ).whenA(calculatedType.value =!= argumentDefinition.typeDefinition.typeName.value)
+                              }
+                              .sequence_
+                              .liftOptionT
+                          }
+  } yield functionDefinition.definition.typeDefinition.typeName.value
 }
