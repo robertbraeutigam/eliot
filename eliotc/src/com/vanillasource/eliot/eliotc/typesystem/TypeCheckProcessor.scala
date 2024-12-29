3571d8712d2ff39fc3fea2b05db37c2f8178c05f
Robert Brautigam <robert.brautigam@prewave.ai>
2024-12-29 20:54:23 +0100

    Fix type checker

--- eliotc/src/com/vanillasource/eliot/eliotc/typesystem/TypeCheckProcessor.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/typesystem/TypeCheckProcessor.scala
@@ -11,6 +11,7 @@ import com.vanillasource.eliot.eliotc.resolve.FunctionBody.NonNative
 import com.vanillasource.eliot.eliotc.resolve.{Expression, FunctionDefinition, ResolvedFunction, TypeDefinition}
 import com.vanillasource.eliot.eliotc.source.Sourced
 import com.vanillasource.eliot.eliotc.source.SourcedError.compilerError
+import com.vanillasource.eliot.eliotc.typesystem.TypeCheckProcessor.TypeCheck
 import com.vanillasource.eliot.eliotc.{CompilationProcess, CompilerFact, CompilerProcessor}
 import com.vanillasource.util.CatsOps.*
 
@@ -31,53 +32,66 @@ class TypeCheckProcessor extends CompilerProcessor with Logging {
   )(using process: CompilationProcess): OptionT[IO, Unit] = {
     for {
       topType <- checkCallTypes(body)
-      _       <- if (topType.value === typeDefinition.typeName.value) {
-                   process.registerFact(TypeCheckedFunction(ffqn, functionDefinition)).liftOptionT
+      _       <- if (topType.typeName.value === typeDefinition.typeName.value) {
+                   if (topType.success) {
+                     process.registerFact(TypeCheckedFunction(ffqn, functionDefinition)).liftOptionT
+                   } else {
+                     OptionT.none[IO, Unit]
+                   }
                  } else {
                    compilerError(
-                     topType.as(
-                       s"Expression type is ${topType.value}, but function declared to return ${typeDefinition.typeName.value}"
+                     topType.typeName.as(
+                       s"Expression type is ${topType.typeName.value}, but function declared to return ${typeDefinition.typeName.value}"
                      )
                    ).liftOptionT
                  }
     } yield ()
   }
 
-  // TODO: just return case class with type name and whether there was an error!
-  private def checkCallTypes(expression: Tree[Expression])(using CompilationProcess): OptionT[IO, Sourced[String]] =
+  private def checkCallTypes(expression: Tree[Expression])(using CompilationProcess): OptionT[IO, TypeCheck] =
     expression match
       case Tree.Empty()                                       => OptionT.none
       case Tree.Node(FunctionApplication(sourcedFfqn), nodes) =>
         for {
           argumentTypes <- nodes.map(checkCallTypes).sequence
-          returnType    <- checkCallType(sourcedFfqn, argumentTypes)
-        } yield sourcedFfqn.as(returnType)
-      case Tree.Node(IntegerLiteral(value), _)                => OptionT.pure(value.as("Byte")) // TODO: Hardcoded for now
+          typeCheck     <- checkCallType(sourcedFfqn, argumentTypes.map(_.typeName))
+        } yield TypeCheck(argumentTypes.map(_.success).fold(true)(_ & _) & typeCheck.success, typeCheck.typeName)
+      case Tree.Node(IntegerLiteral(value), _)                =>
+        OptionT.pure(TypeCheck(true, value.as("Byte"))) // TODO: Hardcoded for now
 
-  // TODO: issue error if call was not resolved! could be a random function that is not defined
+  // TODO: issue error if call was not resolved! could be a random function that is not defined, TEST!
   private def checkCallType(sourcedFfqn: Sourced[FunctionFQN], calculatedArgumentTypes: Seq[Sourced[String]])(using
       process: CompilationProcess
-  ): OptionT[IO, String] = for {
+  ): OptionT[IO, TypeCheck] = for {
     functionDefinition <- process.getFact(ResolvedFunction.Key(sourcedFfqn.value)).toOptionT
-    _                  <- if (calculatedArgumentTypes.length =!= functionDefinition.definition.arguments.length) {
+    success            <- if (calculatedArgumentTypes.length =!= functionDefinition.definition.arguments.length) {
                             compilerError(
                               sourcedFfqn.as(
                                 s"Function is called with ${calculatedArgumentTypes.length} parameters, but needs ${functionDefinition.definition.arguments.length}."
                               )
-                            ).liftOptionT
+                            ).as(false).liftOptionT
                           } else {
                             // Check argument types one by one
                             calculatedArgumentTypes
                               .zip(functionDefinition.definition.arguments)
                               .map { (calculatedType, argumentDefinition) =>
-                                compilerError(
-                                  calculatedType.as(
-                                    s"Expression had type ${calculatedType.value}, but needed: ${argumentDefinition.typeDefinition.typeName.value}"
-                                  )
-                                ).whenA(calculatedType.value =!= argumentDefinition.typeDefinition.typeName.value)
+                                if (calculatedType.value === argumentDefinition.typeDefinition.typeName.value) {
+                                  true.pure[IO]
+                                } else {
+                                  compilerError(
+                                    calculatedType.as(
+                                      s"Expression had type ${calculatedType.value}, but needed: ${argumentDefinition.typeDefinition.typeName.value}"
+                                    )
+                                  ).as(false)
+                                }
                               }
-                              .sequence_
+                              .sequence
+                              .map(_.fold(true)(_ & _)) // True, iff all checks are success
                               .liftOptionT
                           }
-  } yield functionDefinition.definition.typeDefinition.typeName.value
+  } yield TypeCheck(success, sourcedFfqn.as(functionDefinition.definition.typeDefinition.typeName.value))
+}
+
+object TypeCheckProcessor {
+  private case class TypeCheck(success: Boolean, typeName: Sourced[String])
 }
