e20831cf48b59623574d1f5d75e9898eb1bb6544
Robert Brautigam <robert.brautigam@prewave.ai>
2025-01-31 17:00:37 +0100

    Move its parsing to AST

--- eliotc/src/com/vanillasource/eliot/eliotc/ast/AST.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/ast/AST.scala
@@ -2,6 +2,12 @@ package com.vanillasource.eliot.eliotc.ast
 
 import cats.Show
 import cats.syntax.all.*
+import com.vanillasource.eliot.eliotc.ast.ASTComponent.component
+import com.vanillasource.eliot.eliotc.ast.Primitives.*
+import com.vanillasource.eliot.eliotc.source.Sourced
+import com.vanillasource.eliot.eliotc.token.Token
+import com.vanillasource.parser.Parser
+import com.vanillasource.parser.Parser.*
 
 case class AST(
     importStatements: Seq[ImportStatement],
@@ -16,4 +22,21 @@ object AST {
         .mkString(", ")}, function definitions: ${ast.functionDefinitions
         .map(_.show)
         .mkString(", ")}, type definitions: ${ast.typeDefinitions.map(_.show).mkString(", ")}"
+
+  given ASTComponent[AST] = new ASTComponent[AST] {
+    override def parser: Parser[Sourced[Token], AST] = for {
+      importStatements <-
+        component[ImportStatement]
+          .attemptPhraseTo(topLevel.void or endOfInput())
+          .anyTimesWhile(topLevelKeyword("import").find())
+          .map(_.flatten)
+      definitions      <-
+        (component[FunctionDefinition] xor component[DataDefinition])
+          .attemptPhraseTo(topLevel.void or endOfInput())
+          .anyTimesWhile(any())
+          .map(_.flatten)
+    } yield AST(importStatements, definitions.flatMap(_.left.toSeq), definitions.flatMap(_.toSeq))
+
+    private def topLevel = acceptIf(isTopLevel, "top level definition")
+  }
 }
--- eliotc/src/com/vanillasource/eliot/eliotc/ast/TokenParser.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/ast/TokenParser.scala
@@ -1,33 +1,15 @@
 package com.vanillasource.eliot.eliotc.ast
 
 import cats.Show
-import cats.data.IndexedStateT
-import cats.syntax.all.*
 import com.vanillasource.eliot.eliotc.ast.ASTComponent.component
-import com.vanillasource.eliot.eliotc.ast.Primitives.*
 import com.vanillasource.eliot.eliotc.source.Sourced
 import com.vanillasource.eliot.eliotc.token.Token
 import com.vanillasource.eliot.eliotc.token.Token.{Identifier, Keyword, Symbol}
+import com.vanillasource.parser.Parser
 import com.vanillasource.parser.Parser.*
-import com.vanillasource.parser.{Parser, ParserResult}
 
 object TokenParser {
-  lazy val astParser: Parser[Sourced[Token], AST] = {
-    for {
-      importStatements <-
-        component[ImportStatement]
-          .attemptPhraseTo(topLevel.void or endOfInput())
-          .anyTimesWhile(topLevelKeyword("import").find())
-          .map(_.flatten)
-      definitions      <-
-        (component[FunctionDefinition] xor component[DataDefinition])
-          .attemptPhraseTo(topLevel.void or endOfInput())
-          .anyTimesWhile(any())
-          .map(_.flatten)
-    } yield AST(importStatements, definitions.flatMap(_.left.toSeq), definitions.flatMap(_.toSeq))
-  }.fully()
-
-  private def topLevel = acceptIf(isTopLevel, "top level definition")
+  lazy val astParser: Parser[Sourced[Token], AST] = component[AST].fully()
 
   given Show[Token] = {
     case Identifier(content)           => s"identifier '$content'"
