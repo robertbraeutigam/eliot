4fdfd64b46d2488e0ebe7ce306fd09815c70607a
Robert Brautigam <robert.brautigam@prewave.ai>
2025-02-05 16:51:00 +0100

    Unification missing

--- eliotc/src/com/vanillasource/eliot/eliotc/typesystem/TypeInference.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/typesystem/TypeInference.scala
@@ -10,7 +10,13 @@ class TypeInference private (
     scope: Ref[CompilationIO, Map[String, TypeInference]],
     equalTo: Ref[CompilationIO, Seq[TypeInference]]
 ) {
-  def receivesFrom(typeReference: TypeReference): CompilationIO[TypeInference] = ???
+  def receivesFrom(typeReference: TypeReference): CompilationIO[TypeInference] = for {
+    emptyScope  <- Ref.of[CompilationIO, Map[String, TypeInference]](Map.empty)
+    newEqualTo  <- Ref.of[CompilationIO, Seq[TypeInference]](Seq(this))
+    newInference = TypeInference(typeReference, emptyScope, newEqualTo)
+    // TODO: unifications here
+    _           <- equalTo.update(newInference +: _)
+  } yield newInference
 
   def inferTypeFor(typeReference: TypeReference): CompilationIO[TypeInference] = typeReference match
     case TypeReference.DirectTypeReference(_)     => newSameScopeInference(typeReference)
