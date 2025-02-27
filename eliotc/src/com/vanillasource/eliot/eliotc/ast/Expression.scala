0eca1aef480ee60f7632c1ca400b5420d37ccbc6
Robert Brautigam <robert.brautigam@prewave.ai>
2025-02-27 16:35:09 +0100

    Cleanup

--- eliotc/src/com/vanillasource/eliot/eliotc/ast/Expression.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/ast/Expression.scala
@@ -1,6 +1,7 @@
 package com.vanillasource.eliot.eliotc.ast
 
 import cats.Show
+import cats.syntax.all.*
 import com.vanillasource.eliot.eliotc.ast.Primitives.*
 import com.vanillasource.eliot.eliotc.source.Sourced
 import com.vanillasource.eliot.eliotc.token.Token
@@ -13,12 +14,11 @@ object Expression {
   case class FunctionApplication(functionName: Sourced[Token], arguments: Seq[Expression]) extends Expression
   case class IntegerLiteral(integerLiteral: Sourced[Token])                                extends Expression
 
-  given Show[Expression] = new Show[Expression] {
-    override def show(e: Expression): String = e match
-      case IntegerLiteral(Sourced(_, _, value))                   => value.toString
-      case FunctionApplication(Sourced(_, _, value), ns @ x :: _) =>
-        s"${value.toString}(${ns.map(show).mkString(", ")})"
-      case FunctionApplication(Sourced(_, _, value), _)           => value.toString
+  given Show[Expression] = {
+    case IntegerLiteral(Sourced(_, _, value))                   => value.toString
+    case FunctionApplication(Sourced(_, _, value), ns @ x :: _) =>
+      s"${value.toString}(${ns.map(_.show).mkString(", ")})"
+    case FunctionApplication(Sourced(_, _, value), _)           => value.toString
   }
 
   given ASTComponent[Expression] = new ASTComponent[Expression] {
