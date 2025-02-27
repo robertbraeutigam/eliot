e1321f65acff0b5f7eec724df30c75e16c84fc25
Robert Brautigam <robert.brautigam@prewave.ai>
2025-02-27 17:02:05 +0100

    Add function literals

--- eliotc/src/com/vanillasource/eliot/eliotc/ast/Expression.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/ast/Expression.scala
@@ -2,6 +2,7 @@ package com.vanillasource.eliot.eliotc.ast
 
 import cats.Show
 import cats.syntax.all.*
+import com.vanillasource.eliot.eliotc.ast.ASTComponent.component
 import com.vanillasource.eliot.eliotc.ast.Primitives.*
 import com.vanillasource.eliot.eliotc.source.Sourced
 import com.vanillasource.eliot.eliotc.token.Token
@@ -12,23 +13,33 @@ sealed trait Expression
 
 object Expression {
   case class FunctionApplication(functionName: Sourced[Token], arguments: Seq[Expression]) extends Expression
+  case class FunctionLiteral(parameters: Seq[ArgumentDefinition], body: Expression)        extends Expression
   case class IntegerLiteral(integerLiteral: Sourced[Token])                                extends Expression
 
   given Show[Expression] = {
     case IntegerLiteral(Sourced(_, _, value))                   => value.toString
-    case FunctionApplication(Sourced(_, _, value), ns @ x :: _) =>
+    case FunctionApplication(Sourced(_, _, value), ns @ _ :: _) =>
       s"${value.toString}(${ns.map(_.show).mkString(", ")})"
     case FunctionApplication(Sourced(_, _, value), _)           => value.toString
+    case FunctionLiteral(parameters, body)                      => parameters.map(_.show).mkString("(", ", ", ")") + " -> " + body.show
   }
 
   given ASTComponent[Expression] = new ASTComponent[Expression] {
     override def parser: Parser[Sourced[Token], Expression] =
-      functionApplication or integerLiteral
+      functionLiteral or functionApplication or integerLiteral
 
     private val functionApplication: Parser[Sourced[Token], Expression] = for {
       name <- acceptIf(isIdentifier, "function name")
       args <- argumentListOf(parser)
     } yield FunctionApplication(name, args)
+
+    private val functionLiteral: Parser[Sourced[Token], Expression] = for {
+      parameters <-
+        bracketedCommaSeparatedItems("(", component[ArgumentDefinition], ")") or
+          component[ArgumentDefinition].map(Seq(_))
+      _          <- symbol("->")
+      body       <- parser
+    } yield FunctionLiteral(parameters, body)
 
     private val integerLiteral: Parser[Sourced[Token], Expression] = for {
       lit <- acceptIf(isIntegerLiteral, "integer literal")
--- eliotc/test/src/com/vanillasource/eliot/eliotc/ast/ASTParserTest.scala
+++ eliotc/test/src/com/vanillasource/eliot/eliotc/ast/ASTParserTest.scala
@@ -98,7 +98,7 @@ class ASTParserTest extends ProcessorTest(new Tokenizer(), new ASTParser()) {
 
   it should "reject a function application with empty parameter list" in {
     runEngineForErrors("a: Byte = b()").asserting(
-      _ shouldBe Seq("Expected function name or integer literal, but encountered symbol ')'.")
+      _ shouldBe Seq("Expected symbol '(', symbol '->', function name or integer literal, but encountered symbol ')'.")
     )
   }
 
