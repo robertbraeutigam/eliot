edba13117851cbfce14e09f43ea0ead2c1c80fed
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2025-03-26 10:26:05 +0100

    Add unique names

--- eliotc/src/com/vanillasource/eliot/eliotc/typesystem/UniqueGenericNames.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/typesystem/UniqueGenericNames.scala
@@ -1,18 +1,31 @@
 package com.vanillasource.eliot.eliotc.typesystem
 
+import cats.syntax.all.*
 import cats.Monad
 import cats.data.StateT
+import com.vanillasource.eliot.eliotc.resolve.fact.TypeReference.{DirectTypeReference, GenericTypeReference}
 import com.vanillasource.eliot.eliotc.resolve.fact.{ArgumentDefinition, TypeReference}
 
 import scala.annotation.tailrec
 
-case class UniqueGenericNames(nextNameIndex: Int = 0, boundNames: Map[String, TypeReference] = Map.empty) {
+// TODO: This is 3 things in one, split this up
+case class UniqueGenericNames(
+    nextNameIndex: Int = 0,
+    boundNames: Map[String, TypeReference] = Map.empty,
+    cache: Map[String, TypeReference] = Map.empty
+) {
   def generateCurrentName(): String = generateName(nextNameIndex, "")
 
-  def advanceNameIndex(): UniqueGenericNames = UniqueGenericNames(nextNameIndex + 1, boundNames)
+  def advanceNameIndex(): UniqueGenericNames = UniqueGenericNames(nextNameIndex + 1, boundNames, cache)
 
   def boundType(name: String, typeReference: TypeReference): UniqueGenericNames =
-    UniqueGenericNames(nextNameIndex, boundNames + (name -> typeReference))
+    UniqueGenericNames(nextNameIndex, boundNames + (name -> typeReference), cache)
+
+  def addToCache(name: String, typeReference: TypeReference): UniqueGenericNames =
+    UniqueGenericNames(nextNameIndex, boundNames, cache + (name -> typeReference))
+
+  def clearCache(): UniqueGenericNames =
+    UniqueGenericNames(nextNameIndex, boundNames)
 
   @tailrec
   private def generateName(remainingIndex: Int, alreadyGeneratedSuffix: String): String =
@@ -32,8 +45,44 @@ object UniqueGenericNames {
       currentNames <- StateT.get[F, UniqueGenericNames]
     } yield currentNames.boundNames.apply(name)
 
+  def makeUnique[F[_]](typeReference: TypeReference)(using Monad[F]): StateT[F, UniqueGenericNames, TypeReference] =
+    clearCache() >> makeUniqueCached(typeReference)
+
+  private def clearCache[F[_]]()(using Monad[F]): StateT[F, UniqueGenericNames, Unit] =
+    StateT.modifyF[F, UniqueGenericNames](s => Monad[F].pure(s.clearCache()))
+
+  private def makeUniqueCached[F[_]](typeReference: TypeReference)(using
+      Monad[F]
+  ): StateT[F, UniqueGenericNames, TypeReference] =
+    typeReference match
+      case DirectTypeReference(dataType, genericParameters) =>
+        genericParameters.traverse(makeUnique).map(DirectTypeReference(dataType, _))
+      case GenericTypeReference(name, genericParameters)    =>
+        for {
+          currentNames        <- StateT.get[F, UniqueGenericNames]
+          uniqueTypeReference <- currentNames.cache.get(name.value) match
+                                   case Some(cachedTypeReference) =>
+                                     cachedTypeReference.pure[[T] =>> StateT[F, UniqueGenericNames, T]]
+                                   case None                      =>
+                                     for {
+                                       uniqueParameters   <- genericParameters.traverse(makeUnique)
+                                       currentNames       <- StateT.get[F, UniqueGenericNames]
+                                       uniqueName          = currentNames.generateCurrentName()
+                                       uniqueTypeReference = GenericTypeReference(name.as(uniqueName), uniqueParameters)
+                                       _                  <-
+                                         StateT.set(
+                                           currentNames.advanceNameIndex().addToCache(uniqueName, uniqueTypeReference)
+                                         )
+                                     } yield uniqueTypeReference
+        } yield uniqueTypeReference
+
   def boundType[F[_]](arg: ArgumentDefinition)(using Monad[F]): StateT[F, UniqueGenericNames, Unit] =
-    StateT.modifyF[F, UniqueGenericNames](names => Monad[F].pure(names.boundType(arg.name.value, arg.typeReference)))
+    for {
+      uniqueTypeReference <- makeUnique(arg.typeReference)
+      _                   <- StateT.modifyF[F, UniqueGenericNames](names =>
+                               Monad[F].pure(names.boundType(arg.name.value, uniqueTypeReference))
+                             )
+    } yield ()
 
   def generateNextUniqueName[F[_]]()(using Monad[F]): StateT[F, UniqueGenericNames, String] =
     for {
