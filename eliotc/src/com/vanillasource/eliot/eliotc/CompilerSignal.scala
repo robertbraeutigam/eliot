9e8e61d4a13274b7223c263e0b1e83c129a278d1
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2025-11-15 15:40:12 +0100

    Remove unused import

--- eliotc/src/com/vanillasource/eliot/eliotc/CompilerSignal.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/CompilerSignal.scala
@@ -1,7 +1,5 @@
 package com.vanillasource.eliot.eliotc
 
-import com.vanillasource.eliot.eliotc.CompilerFact
-
 /** Signal is a fact that doesn't have any content on top of its key. In other words, its mere existence or absence is
   * all the information it carries.
   */
--- eliotc/src/com/vanillasource/eliot/eliotc/Init.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/Init.scala
@@ -1,7 +1,5 @@
 package com.vanillasource.eliot.eliotc
 
-import com.vanillasource.eliot.eliotc.CompilerSignal
-
 /** Inserted when the compiler is started as the first and only automatically inserted fact. This fact can be used to
   * bootstrap initial processors.
   */
