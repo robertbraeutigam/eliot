4c71b25021a7d5ad7fd996d9985e647fc00c0c70
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2025-12-01 12:24:04 +0100

    Multi module build

---
+++ jvm/src/com/vanillasource/eliot/eliotc/jvm/GenerateModule.scala
@@ -0,0 +1,17 @@
+package com.vanillasource.eliot.eliotc.jvm
+
+import com.vanillasource.eliot.eliotc.{CompilerFact, CompilerFactKey}
+import com.vanillasource.eliot.eliotc.module.fact.{FunctionFQN, ModuleName, TypeFQN}
+import com.vanillasource.eliot.eliotc.source.pos.Sourced
+
+case class GenerateModule(
+    moduleName: ModuleName,
+    usedFunctions: Seq[Sourced[FunctionFQN]],
+    usedTypes: Seq[Sourced[TypeFQN]]
+) extends CompilerFact {
+  override def key(): CompilerFactKey[GenerateModule] = GenerateModule.Key(moduleName)
+}
+
+object GenerateModule {
+  case class Key(moduleName: ModuleName) extends CompilerFactKey[GenerateModule]
+}
---
+++ jvm/src/com/vanillasource/eliot/eliotc/jvm/NativeType.scala
@@ -0,0 +1,41 @@
+package com.vanillasource.eliot.eliotc.jvm
+
+import cats.syntax.all.*
+import com.vanillasource.eliot.eliotc.module.fact.TypeFQN.systemLangType
+import com.vanillasource.eliot.eliotc.module.fact.{ModuleName, TypeFQN}
+
+trait NativeType {
+  def javaSignatureName: String
+}
+
+object NativeType {
+  val types: Map[TypeFQN, NativeType] = Map.from(
+    Seq(
+      (systemLangType("String"), eliot_lang_String),
+      (systemLangType("Function"), eliot_lang_Function),
+      (systemLangType("Unit"), eliot_lang_Unit)
+    )
+  )
+
+  def javaSignatureName(typeFqn: TypeFQN): String =
+    types.get(typeFqn).map(_.javaSignatureName).getOrElse(convertToJavaName(typeFqn))
+
+  def convertToJavaName(typeFQN: TypeFQN): String =
+    // All data classes are nested classes inside the class denoted by the "module"!
+    "L" + convertToMainClassName(typeFQN.moduleName) + "$" + typeFQN.typeName + ";"
+
+  def convertToMainClassName(moduleName: ModuleName): String =
+    moduleName.packages.appended(moduleName.name).mkString("/")
+
+  private def eliot_lang_String: NativeType = new NativeType {
+    override def javaSignatureName: String = "Ljava/lang/String;"
+  }
+
+  private def eliot_lang_Unit: NativeType = new NativeType {
+    override def javaSignatureName: String = "V"
+  }
+
+  private def eliot_lang_Function: NativeType = new NativeType {
+    override def javaSignatureName: String = "Ljava/lang/Function;"
+  }
+}
