73789572dc9900801931d5f9aa1739b393c5ed78
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2025-12-02 20:55:24 +0100

    Change to commands

--- jvm/src/com/vanillasource/eliot/eliotc/jvm/JvmLayer.scala
+++ jvm/src/com/vanillasource/eliot/eliotc/jvm/JvmLayer.scala
@@ -3,7 +3,6 @@ package com.vanillasource.eliot.eliotc.jvm
 import cats.data.StateT
 import cats.effect.IO
 import com.vanillasource.eliot.eliotc.CompilerProcessor
-import com.vanillasource.eliot.eliotc.base.BaseLayer
 import com.vanillasource.eliot.eliotc.layer.Configuration.namedKey
 import com.vanillasource.eliot.eliotc.layer.{Configuration, Layer}
 import com.vanillasource.eliot.eliotc.main.Main
@@ -18,9 +17,18 @@ class JvmLayer extends Layer {
   private val mainKey = namedKey[FunctionFQN]("mainFunction")
 
   override def commandLineParser(): OParser[_, Configuration] = OParser.sequence(
-    opt[String]('m', "main-module")
-      .text("module that has a suitable main method")
-      .action((moduleName, config) => config.set(mainKey, FunctionFQN(ModuleName.parse(moduleName), "main")))
+    cmd("jvm")
+      .text("target the jvm backend")
+      .children(
+        cmd("exe-jar")
+          .text("generate executable jar")
+          .children(
+            opt[String]('m', "main-module")
+              .required()
+              .text("module that has a suitable main method")
+              .action((moduleName, config) => config.set(mainKey, FunctionFQN(ModuleName.parse(moduleName), "main")))
+          )
+      )
   )
 
   override def initialize(configuration: Configuration): StateT[IO, CompilerProcessor, Unit] =
