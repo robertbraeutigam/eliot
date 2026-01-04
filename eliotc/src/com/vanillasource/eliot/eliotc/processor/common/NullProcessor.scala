bcb2c458a9da1ac310d8d3ff2178f41010114f0c
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-01-04 18:56:07 +0100

    Refactor

--- eliotc/src/com/vanillasource/eliot/eliotc/processor/common/NullProcessor.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/processor/common/NullProcessor.scala
@@ -2,10 +2,12 @@ package com.vanillasource.eliot.eliotc.processor.common
 
 import cats.Monad
 import cats.effect.IO
-import cats.syntax.all.*
 import com.vanillasource.eliot.eliotc.processor.CompilerIO.CompilerIO
 import com.vanillasource.eliot.eliotc.processor.{CompilerFactKey, CompilerProcessor}
 
+/**
+ * Does nothing.
+ */
 class NullProcessor extends CompilerProcessor {
   override def generate(factKey: CompilerFactKey[?]): CompilerIO[Unit] = Monad[CompilerIO].unit
 }
