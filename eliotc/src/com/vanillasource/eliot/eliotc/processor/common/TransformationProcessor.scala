009122398c535e852a304ed17aa71804922b7908
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-01-04 21:56:18 +0100

    Refactor

--- eliotc/src/com/vanillasource/eliot/eliotc/processor/common/TransformationProcessor.scala
+++ eliotc/src/com/vanillasource/eliot/eliotc/processor/common/TransformationProcessor.scala
@@ -2,20 +2,17 @@ package com.vanillasource.eliot.eliotc.processor.common
 
 import com.vanillasource.eliot.eliotc.processor.CompilerIO.CompilerIO
 import com.vanillasource.eliot.eliotc.processor.{CompilerFact, CompilerFactKey}
-
 import scala.annotation.unused
 import scala.reflect.ClassTag
+import com.vanillasource.eliot.eliotc.processor.CompilerIO.*
 
 abstract class TransformationProcessor[V <: CompilerFact, I <: CompilerFactKey[V], O <: CompilerFactKey[?]](
     keyTransition: O => I
 )(using ct: ClassTag[O])
     extends SingleKeyTypeProcessor[O] {
 
-  protected def generateFact(requestedKey: O): CompilerIO[Unit] = {
-    import com.vanillasource.eliot.eliotc.processor.CompilerIO.*
-    val key = keyTransition(requestedKey)
-    getFactOrAbort(key).flatMap(fact => generateFromKeyAndFact(requestedKey, fact))
-  }
+  protected def generateFact(requestedKey: O): CompilerIO[Unit] =
+    getFactOrAbort(keyTransition(requestedKey)).flatMap(fact => generateFromKeyAndFact(requestedKey, fact))
 
   def generateFromFact(@unused fact: V): CompilerIO[Unit] = ???
 
