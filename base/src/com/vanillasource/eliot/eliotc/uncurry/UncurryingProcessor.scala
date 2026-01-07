229be0eeee45e508404b1111111a17093c838d84
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-01-07 10:23:58 +0100

    Implement UncurryingProcessor

--- base/src/com/vanillasource/eliot/eliotc/uncurry/UncurryingProcessor.scala
+++ base/src/com/vanillasource/eliot/eliotc/uncurry/UncurryingProcessor.scala
@@ -14,6 +14,7 @@ import com.vanillasource.eliot.eliotc.uncurry.{
   UncurriedTypedExpression,
   UncurriedTypedFunctionDefinition
 }
+import scala.annotation.tailrec
 
 /** Processor that reverses the currying transformation applied during resolution.
   *
@@ -40,15 +41,17 @@ class UncurryingProcessor
       typeCheckedFunction: TypeCheckedFunction
   ): CompilerIO[UncurriedFunction] =
     for {
-      // Determine the original arity of function
-      originalArity            <- getFactOrAbort(UnifiedModuleFunction.Key(key.ffqn)).map(_.functionDefinition.args.length)
+      // Get the original function definition to determine arity and parameter names
+      unifiedFunction          <- getFactOrAbort(UnifiedModuleFunction.Key(key.ffqn))
+      originalArity            = unifiedFunction.functionDefinition.args.length
       // Restore original arity
       (args, returnType, body) <-
         restoreArity(
           typeCheckedFunction.definition.name,
           typeCheckedFunction.definition.valueType,
           typeCheckedFunction.definition.body,
-          originalArity
+          originalArity,
+          unifiedFunction.functionDefinition.args
         )
     } yield UncurriedFunction(
       typeCheckedFunction.ffqn,
@@ -65,7 +68,8 @@ class UncurryingProcessor
       functionName: Sourced[String],
       typeReference: TypeReference,
       body: Option[Sourced[TypedExpression]],
-      arity: Int
+      arity: Int,
+      originalArgs: Seq[com.vanillasource.eliot.eliotc.ast.fact.ArgumentDefinition]
   ): CompilerIO[(Seq[ArgumentDefinition], TypeReference, Option[Sourced[UncurriedTypedExpression]])] =
     if (arity === 0) {
       (Seq.empty, typeReference, body.map(_.map(convertExpression))).pure[CompilerIO]
@@ -73,12 +77,89 @@ class UncurryingProcessor
       typeReference match {
         case TypeReference.DirectTypeReference(Sourced(_, _, systemFunctionType), Seq(inputType, outputType)) =>
           // Needs to be a function to deconstruct
-          ???
-        case _                                                                                                =>
+          // Get the current parameter index (total args - remaining arity)
+          val paramIndex = originalArgs.length - arity
+          val originalArg = originalArgs(paramIndex)
+
+          body match {
+            case Some(Sourced(start, end, TypedExpression(_, TypedExpression.FunctionLiteral(parameter, innerBody)))) =>
+              // Function with body - extract parameter from body
+              restoreArity(functionName, outputType, Some(innerBody), arity - 1, originalArgs).map {
+                case (restArgs, restType, restBody) =>
+                  (parameter +: restArgs, restType, restBody)
+              }
+            case None =>
+              // Function without body - use parameter name from original AST with type from curried signature
+              val parameter = ArgumentDefinition(originalArg.name, inputType)
+              restoreArity(functionName, outputType, None, arity - 1, originalArgs).map { case (restArgs, restType, restBody) =>
+                (parameter +: restArgs, restType, restBody)
+              }
+            case _ =>
+              compilerAbort(functionName.as(s"Expected function literal in body while restoring arity."))
+          }
+        case _ =>
           compilerAbort(functionName.as(s"Could not restore arity to function."))
       }
     }
 
   private def convertExpression(expr: TypedExpression): UncurriedTypedExpression =
-    ???
+    expr match {
+      case TypedExpression(exprType, TypedExpression.FunctionApplication(target, argument)) =>
+        // Flatten curried applications: ((f a) b) c -> f(a, b, c)
+        val (finalTarget, arguments) = flattenApplication(target, Seq(argument))
+        UncurriedTypedExpression(
+          exprType,
+          UncurriedTypedExpression.FunctionApplication(
+            finalTarget.map(convertExpression),
+            arguments.map(_.map(convertExpression))
+          )
+        )
+
+      case TypedExpression(exprType, TypedExpression.FunctionLiteral(parameter, body)) =>
+        // Flatten nested lambdas: a -> b -> c -> body into (a, b, c) -> body
+        val (parameters, finalBody) = flattenLambda(Seq(parameter), body)
+        UncurriedTypedExpression(
+          exprType,
+          UncurriedTypedExpression.FunctionLiteral(
+            parameters,
+            finalBody.map(convertExpression)
+          )
+        )
+
+      case TypedExpression(exprType, TypedExpression.IntegerLiteral(intLit)) =>
+        UncurriedTypedExpression(exprType, UncurriedTypedExpression.IntegerLiteral(intLit))
+
+      case TypedExpression(exprType, TypedExpression.StringLiteral(strLit)) =>
+        UncurriedTypedExpression(exprType, UncurriedTypedExpression.StringLiteral(strLit))
+
+      case TypedExpression(exprType, TypedExpression.ParameterReference(paramName)) =>
+        UncurriedTypedExpression(exprType, UncurriedTypedExpression.ParameterReference(paramName))
+
+      case TypedExpression(exprType, TypedExpression.ValueReference(valueName)) =>
+        UncurriedTypedExpression(exprType, UncurriedTypedExpression.ValueReference(valueName))
+    }
+
+  @tailrec
+  private def flattenApplication(
+      target: Sourced[TypedExpression],
+      arguments: Seq[Sourced[TypedExpression]]
+  ): (Sourced[TypedExpression], Seq[Sourced[TypedExpression]]) =
+    target.value match {
+      case TypedExpression(_, TypedExpression.FunctionApplication(innerTarget, innerArgument)) =>
+        flattenApplication(innerTarget, innerArgument +: arguments)
+      case _ =>
+        (target, arguments)
+    }
+
+  @tailrec
+  private def flattenLambda(
+      parameters: Seq[ArgumentDefinition],
+      body: Sourced[TypedExpression]
+  ): (Seq[ArgumentDefinition], Sourced[TypedExpression]) =
+    body.value match {
+      case TypedExpression(_, TypedExpression.FunctionLiteral(parameter, innerBody)) =>
+        flattenLambda(parameters :+ parameter, innerBody)
+      case _ =>
+        (parameters, body)
+    }
 }
