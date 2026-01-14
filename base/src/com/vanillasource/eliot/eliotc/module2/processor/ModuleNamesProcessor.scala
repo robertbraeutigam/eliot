c669bad6bd55ba88e3cf1fce6a56e767e06a03a2
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-01-14 13:02:07 +0100

    Add ModuleNames processor

--- base/src/com/vanillasource/eliot/eliotc/module2/processor/ModuleNamesProcessor.scala
+++ base/src/com/vanillasource/eliot/eliotc/module2/processor/ModuleNamesProcessor.scala
@@ -1,14 +1,28 @@
 package com.vanillasource.eliot.eliotc.module2.processor
 
-import com.vanillasource.eliot.eliotc.core.fact.CoreAST
+import cats.syntax.all.*
+import com.vanillasource.eliot.eliotc.core.fact.{CoreAST, NamedValue}
+import com.vanillasource.eliot.eliotc.module2.fact.ModuleNames
 import com.vanillasource.eliot.eliotc.processor.CompilerIO.*
 import com.vanillasource.eliot.eliotc.processor.common.TransformationProcessor
-import com.vanillasource.eliot.eliotc.module2.fact.ModuleNames
+import com.vanillasource.eliot.eliotc.source.content.Sourced.compilerError
 
 class ModuleNamesProcessor extends TransformationProcessor[CoreAST.Key, ModuleNames.Key](key => CoreAST.Key(key.file)) {
 
   override protected def generateFromKeyAndFact(
       key: ModuleNames.Key,
       fact: CoreAST
-  ): CompilerIO[ModuleNames] = ???
+  ): CompilerIO[ModuleNames] =
+    extractNames(fact.ast.value.namedValues).map(names => ModuleNames(key.file, names))
+
+  private def extractNames(namedValues: Seq[NamedValue]): CompilerIO[Set[String]] =
+    namedValues.foldLeftM(Set.empty[String]) { (acc, nv) =>
+      val name = nv.name.value
+
+      if (acc.contains(name)) {
+        compilerError(nv.name.as("Name was already defined in this module.")).as(acc)
+      } else {
+        (acc + name).pure[CompilerIO]
+      }
+    }
 }
