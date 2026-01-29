2e37d15c939ce158962bad11301a2f71e279cf74
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-01-29 22:17:04 +0100

    Refactor

--- base/src/com/vanillasource/eliot/eliotc/eval/fact/Value.scala
+++ base/src/com/vanillasource/eliot/eliotc/eval/fact/Value.scala
@@ -4,7 +4,7 @@ import com.vanillasource.eliot.eliotc.module2.fact.{ModuleName, ValueFQN}
 
 /** The evaluator creates these values to represent every "runtime" value in a program.
   */
-trait Value {
+sealed trait Value {
   def valueType: Value
 }
 
@@ -12,8 +12,8 @@ object Value {
   case class Direct(value: Any, override val valueType: Value)                    extends Value
   case class Structure(fields: Map[String, Value], override val valueType: Value) extends Value
 
-  /** The type of Type itself. This is the root of the type hierarchy where the infinite recursion stops.
-    * Both its valueType and all field types reference itself.
+  /** The type of Type itself. This is the root of the type hierarchy where the infinite recursion stops. Both its
+    * valueType and all field types reference itself.
     */
   object TypeType extends Value {
     lazy val fields: Map[String, Value] = Map(
@@ -22,6 +22,6 @@ object Value {
         TypeType
       )
     )
-    override def valueType: Value = this
+    override def valueType: Value       = this
   }
 }
