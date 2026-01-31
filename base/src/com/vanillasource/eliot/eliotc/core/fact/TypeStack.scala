800195c349f677ca7f96a041c79c6641c3b26992
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-01-31 16:58:43 +0100

    Refactor

---
+++ base/src/com/vanillasource/eliot/eliotc/core/fact/TypeStack.scala
@@ -0,0 +1,34 @@
+package com.vanillasource.eliot.eliotc.core.fact
+
+import cats.syntax.all.*
+import cats.Show
+
+/** A type stack contains type levels for a value. The bottom level (index 0) is the "signature" - the type of the
+  * runtime value. Each subsequent level describes the type of the level below. The implicit top level is always Type.
+  *
+  * For example, for "zero: Int = 0":
+  *   - levels(0) = ValueReference(Int) (the signature)
+  *   - Implicit: Type is the type of Int
+  *
+  * @param levels
+  *   The type levels, from signature (index 0) to higher meta-levels.
+  */
+case class TypeStack[E](levels: Seq[E]) {
+  def signature: Option[E] = levels.headOption
+
+  def levelsWithLevel: Seq[(E, Int)] = levels.zipWithIndex.map { (expr, idx) =>
+    (expr, idx + 1)
+  }
+
+  def maxLevel: Int = levels.length + 1
+}
+
+object TypeStack {
+  def of[E](expression: E): TypeStack[E] = TypeStack(Seq(expression))
+
+  def empty[E]: TypeStack[E] = TypeStack(Seq.empty[E])
+
+  given [E: Show]: Show[TypeStack[E]] = stack =>
+    if (stack.levels.size <= 1) stack.levels.map(_.show).mkString
+    else s"(${stack.levels.reverse.map(_.show).mkString(" :: ")})"
+}
