de01c880e65a19fe957bff9937f7012520ab7842
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-02-10 18:19:57 +0100

    Refactor

--- base/src/com/vanillasource/eliot/eliotc/source/scan/PathScanner.scala
+++ base/src/com/vanillasource/eliot/eliotc/source/scan/PathScanner.scala
@@ -5,13 +5,13 @@ import cats.syntax.all.*
 import com.vanillasource.eliot.eliotc.feedback.Logging
 import com.vanillasource.eliot.eliotc.feedback.User.*
 import com.vanillasource.eliot.eliotc.processor.CompilerIO.*
-import com.vanillasource.eliot.eliotc.processor.common.SingleKeyTypeProcessor
+import com.vanillasource.eliot.eliotc.processor.common.{SingleFactProcessor, SingleKeyTypeProcessor}
 import com.vanillasource.eliot.eliotc.source.stat.FileStat
 
 import java.nio.file.Path
 
-class PathScanner(rootPaths: Seq[Path]) extends SingleKeyTypeProcessor[PathScan.Key] with Logging {
-  override protected def generateFact(key: PathScan.Key): CompilerIO[Unit] =
+class PathScanner(rootPaths: Seq[Path]) extends SingleFactProcessor[PathScan.Key] with Logging {
+  override protected def generateSingleFact(key: PathScan.Key): CompilerIO[PathScan] =
     for {
       contentFacts <- rootPaths
                         .map(_.resolve(key.path).toFile)
@@ -19,11 +19,7 @@ class PathScanner(rootPaths: Seq[Path]) extends SingleKeyTypeProcessor[PathScan.
                         .traverse(file => getFactOrAbort(FileStat.Key(file)))
       files         = contentFacts.filter(_.lastModified.isDefined).map(_.file)
       _            <- debug[CompilerIO](s"Found files: ${files.mkString(", ")}")
-      _            <- if (files.isEmpty) {
-                        compilerGlobalError(s"Could not find path ${key.path} at given roots: ${rootPaths.mkString(", ")}")
-                          .to[CompilerIO]
-                      } else {
-                        registerFactIfClear(PathScan(key.path, files.map(_.toURI)))
-                      }
-    } yield ()
+      _            <- (compilerGlobalError(s"Could not find path ${key.path} at given roots: ${rootPaths.mkString(", ")}")
+                        .to[CompilerIO] >> abort).whenA(files.isEmpty)
+    } yield PathScan(key.path, files.map(_.toURI))
 }
