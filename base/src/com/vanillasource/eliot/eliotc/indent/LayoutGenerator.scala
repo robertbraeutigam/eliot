7aa9a0e868344f87a24cd8def380c3e79ee81ad7
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-02-16 16:20:53 +0100

    Refactor

--- base/src/com/vanillasource/eliot/eliotc/indent/LayoutGenerator.scala
+++ base/src/com/vanillasource/eliot/eliotc/indent/LayoutGenerator.scala
@@ -3,7 +3,7 @@ package com.vanillasource.eliot.eliotc.indent
 import cats.data.State
 import cats.syntax.all.*
 import com.vanillasource.eliot.eliotc.indent.LayoutGenerator.GeneratorState
-import com.vanillasource.eliot.eliotc.indent.LayoutToken.ContentToken
+import com.vanillasource.eliot.eliotc.indent.LayoutToken.*
 import com.vanillasource.eliot.eliotc.pos.Position
 import com.vanillasource.eliot.eliotc.processor.CompilerIO.CompilerIO
 import com.vanillasource.eliot.eliotc.processor.common.TransformationProcessor
@@ -19,66 +19,43 @@ class LayoutGenerator
   ): CompilerIO[SourceLayoutTokens] =
     fact.tokens.value match {
       case head :: tail =>
-        SourceLayoutTokens(
-          key.uri,
-          fact.tokens.as(
-            head
-              .map(ContentToken.apply) +: generateLayout(tail).runA(GeneratorState(head, List(1))).value
-          )
-        )
-          .pure[CompilerIO]
-      case Nil          => SourceLayoutTokens(key.uri, fact.tokens.as(Seq.empty)).pure[CompilerIO]
+        val layout = head.map(ContentToken.apply) +: generateLayout(tail).runA(GeneratorState(head, List(1))).value
+        SourceLayoutTokens(key.uri, fact.tokens.as(layout)).pure[CompilerIO]
+      case Nil          =>
+        SourceLayoutTokens(key.uri, fact.tokens.as(Seq.empty)).pure[CompilerIO]
     }
 
-  def generateLayout(remainingTokens: Seq[Sourced[Token]]): State[GeneratorState, Seq[Sourced[LayoutToken]]] =
-    remainingTokens match {
+  private def generateLayout(tokens: Seq[Sourced[Token]]): State[GeneratorState, Seq[Sourced[LayoutToken]]] =
+    tokens match {
       case head :: tail =>
-        State.get[GeneratorState].flatMap { state =>
-          if (head.range.from.line === state.previousToken.range.from.line) {
-            // Token on same line, just transform
-            generateLayout(tail).map(head.map(ContentToken.apply) +: _)
-          } else {
-            // Token on next line, so determine layout
-            if (head.range.from.col > state.indentStack.last) {
-              // Indent seems to be greater, so indent
-              for {
-                _               <- State.set(GeneratorState(head, state.indentStack.appended(head.range.from.col)))
-                tailTransformed <- generateLayout(tail)
-              } yield Seq(
-                head.as(LayoutToken.Newline),
-                head.as(LayoutToken.Indent),
-                head.map(ContentToken.apply)
-              ) ++ tailTransformed
-            } else if (head.range.from.col < state.indentStack.last) {
-              // Indent is less than previous, so dedent until indent level is found
-              val dedents = state.indentStack.reverse.takeWhile(_ > head.range.from.col)
-
-              for {
-                _               <- State.set(
-                                     GeneratorState(
-                                       head,
-                                       state.indentStack.take(state.indentStack.length - dedents.length)
-                                     )
-                                   )
-                tailTransformed <- generateLayout(tail)
-              } yield Seq(head.as(LayoutToken.Newline)) ++ dedents.as(head.as(LayoutToken.Dedent)) ++ Seq(
-                head.map(ContentToken.apply)
-              ) ++ tailTransformed
-            } else {
-              // Indent is the same, so just newline
-              for {
-                _               <- State.set(GeneratorState(head, state.indentStack))
-                tailTransformed <- generateLayout(tail)
-              } yield Seq(head.as(LayoutToken.Newline), head.map(ContentToken.apply)) ++ tailTransformed
-            }
-          }
-        }
+        for {
+          prefix <- layoutPrefix(head)
+          rest   <- generateLayout(tail)
+        } yield (prefix :+ head.map(ContentToken.apply)) ++ rest
       case Nil          =>
-        // Eof, so generate newline + remaining dedents
         State.get[GeneratorState].map { state =>
-          state.previousToken.as(LayoutToken.Newline) +: state.indentStack.tail
-            .as(state.previousToken.as(LayoutToken.Dedent))
+          state.previousToken.as(Newline) +: state.indentStack.tail.as(state.previousToken.as(Dedent))
         }
+    }
+
+  private def layoutPrefix(token: Sourced[Token]): State[GeneratorState, Seq[Sourced[LayoutToken]]] =
+    State.get[GeneratorState].flatMap { state =>
+      if (token.range.from.line === state.previousToken.range.from.line) {
+        State.pure(Seq.empty)
+      } else {
+        val col     = token.range.from.col
+        val current = state.indentStack.last
+        val (newStack, layoutTokens) =
+          if (col > current) {
+            (state.indentStack.appended(col), Seq(Newline, Indent))
+          } else if (col < current) {
+            val dedentCount = state.indentStack.reverse.takeWhile(_ > col).length
+            (state.indentStack.dropRight(dedentCount), Newline +: List.fill(dedentCount)(Dedent))
+          } else {
+            (state.indentStack, Seq(Newline))
+          }
+        State.set(GeneratorState(token, newStack)).as(layoutTokens.map(token.as(_)))
+      }
     }
 }
 
