0c3f4055089a1b173e17f3c37afdf0dbb71535fe
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-03-02 10:17:31 +0100

    Fix Type return imply type context

--- lang/src/com/vanillasource/eliot/eliotc/core/processor/CoreProcessor.scala
+++ lang/src/com/vanillasource/eliot/eliotc/core/processor/CoreProcessor.scala
@@ -1,7 +1,7 @@
 package com.vanillasource.eliot.eliotc.core.processor
 
 import cats.syntax.all.*
-import com.vanillasource.eliot.eliotc.ast.fact.{FunctionDefinition, SourceAST, Qualifier as AstQualifier}
+import com.vanillasource.eliot.eliotc.ast.fact.{Expression as SourceExpression, FunctionDefinition, SourceAST}
 import com.vanillasource.eliot.eliotc.core.fact.{AST as CoreASTData, *}
 import com.vanillasource.eliot.eliotc.feedback.Logging
 import com.vanillasource.eliot.eliotc.processor.CompilerIO.*
@@ -39,7 +39,10 @@ class CoreProcessor
   private def transformFunction(function: FunctionDefinition): NamedValue = {
     import CoreExpressionConverter.*
     val curriedType  = curriedFunctionType(function.args, function.typeDefinition, function.genericParameters)
-    val isTypeBody   = function.name.value.qualifier == AstQualifier.Type
+    val isTypeBody   = function.typeDefinition.value match {
+      case SourceExpression.FunctionApplication(None, fnName, Seq(), Seq()) => fnName.value == "Type"
+      case _                                                               => false
+    }
     val curriedValue = function.body.map(body => buildCurriedBody(function.args, body, isTypeBody))
     val typeStack    = TypeStack.of(curriedType.value)
     val constraints  = function.genericParameters
