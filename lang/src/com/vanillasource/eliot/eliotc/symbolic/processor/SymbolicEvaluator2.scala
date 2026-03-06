6ecf94fef850ec7d179cd9db9d3dbd23adc0776c
Robert Brautigam <brautigam.robert.gyula@gmail.com>
2026-03-06 09:15:15 +0100

    Refactor

--- lang/src/com/vanillasource/eliot/eliotc/symbolic/processor/SymbolicEvaluator2.scala
+++ lang/src/com/vanillasource/eliot/eliotc/symbolic/processor/SymbolicEvaluator2.scala
@@ -1,7 +1,12 @@
 package com.vanillasource.eliot.eliotc.symbolic.processor
 
+import cats.data.{NonEmptySeq, StateT}
+import cats.syntax.all.*
 import com.vanillasource.eliot.eliotc.eval.fact.ExpressionValue
+import com.vanillasource.eliot.eliotc.eval.fact.ExpressionValue.ConcreteValue
+import com.vanillasource.eliot.eliotc.eval.fact.Value.Type
 import com.vanillasource.eliot.eliotc.operator.fact.OperatorResolvedExpression
+import com.vanillasource.eliot.eliotc.processor.CompilerIO.CompilerIO
 import com.vanillasource.eliot.eliotc.source.content.Sourced
 import com.vanillasource.eliot.eliotc.symbolic.fact.TypedExpression
 import com.vanillasource.eliot.eliotc.symbolic.types.TypeCheckState.TypeGraphIO
@@ -11,16 +16,29 @@ object SymbolicEvaluator2 {
   /** Typecheck the given expression stack and return the typed expression, i.e. the result type and the expression when
     * run producing the result.
     */
-  def typeCheck(
-      expressions: Seq[Sourced[OperatorResolvedExpression]]
-  ): TypeGraphIO[TypedExpression] =
-    ???
+  def typeCheck(expressions: NonEmptySeq[Sourced[OperatorResolvedExpression]]): TypeGraphIO[TypedExpression] =
+    for {
+      // The topmost expression needs to be of type "Type" and there is a topmost expression always
+      top    <- typeCheck(ConcreteValue(Type), expressions.head)
+      // Iterate the rest, every level is the type of the next level
+      result <- expressions.tail.foldLeftM((top, expressions.head)) { (acc, expression) =>
+                  for {
+                    previousLevel   <- StateT.liftF(evaluateToNormalForm(acc._2))
+                    typedExpression <- typeCheck(previousLevel, expression)
+                  } yield (typedExpression, expression)
+                }
+    } yield result._1
 
   /** Evaluate the given expression to its symbolic normal form. The normal form is where all referenced values are
     * substituted, "native" functions are "skipped", all other functions a beta-reduced, and constructors are left as
-    * structure.
+    * structure. TODO: this should be provided by Evaluator.
     */
-  def evaluateToNormalForm(expression: Sourced[OperatorResolvedExpression]): ExpressionValue =
+  def evaluateToNormalForm(expression: Sourced[OperatorResolvedExpression]): CompilerIO[ExpressionValue] =
     ???
 
+  private def typeCheck(
+      resultType: ExpressionValue,
+      expression: Sourced[OperatorResolvedExpression]
+  ): TypeGraphIO[TypedExpression] =
+    ???
 }
--- lang/src/com/vanillasource/eliot/eliotc/symbolic/processor/SymbolicTypeCheckProcessor.scala
+++ lang/src/com/vanillasource/eliot/eliotc/symbolic/processor/SymbolicTypeCheckProcessor.scala
@@ -27,40 +27,44 @@ class SymbolicTypeCheckProcessor
       resolvedValue: OperatorResolvedValue
   ): CompilerIO[TypeCheckedValue] = {
     val typeStack  = resolvedValue.typeStack
-    val typeLevels = typeStack.value.levels.toSeq.map(typeStack.as(_))
+    val typeLevels = typeStack.value.levels.map(typeStack.as(_))
     val vfqnShow   = resolvedValue.vfqn.show
 
     for {
-      (endState, result) <- typeCheck(typeLevels ++ resolvedValue.runtime.toSeq).run(TypeCheckState())
-      _                  <- debug[CompilerIO](s"Constraints (of $vfqnShow): ${endState.constraints.show}")
-      solution           <- ConstraintSolver.solve(endState.constraints, endState.universalVars)
-      _                  <- debug[CompilerIO](s"Solution (of $vfqnShow): ${solution.show}")
-    } yield {
-      val resolvedResult           = result.transformTypes(solution.substitute)
-      val resolvedQualifierParams  = resolveQualifierParams(resolvedValue.name).map(solution.substitute)
-      val (signatureType, runtime) = resolvedValue.runtime match {
-        case Some(_) => (resolvedResult.expressionType, Some(typeStack.as(resolvedResult.expression)))
-        case None    => (evaluateToNormalForm(typeStack.as(typeStack.value.signature)), None)
-      }
-      TypeCheckedValue(
-        resolvedValue.vfqn,
-        resolvedValue.name.as(QualifiedName.from(resolvedValue.name.value, resolvedQualifierParams)),
-        signatureType,
-        runtime
-      )
-    }
+      (endState, result)       <- typeCheck(typeLevels.appendSeq(resolvedValue.runtime.toSeq)).run(TypeCheckState())
+      _                        <- debug[CompilerIO](s"Constraints (of $vfqnShow): ${endState.constraints.show}")
+      solution                 <- ConstraintSolver.solve(endState.constraints, endState.universalVars)
+      _                        <- debug[CompilerIO](s"Solution (of $vfqnShow): ${solution.show}")
+      (signatureType, runtime) <- resolvedValue.runtime match {
+                                    case Some(_) =>
+                                      (
+                                        result.transformTypes(solution.substitute).expressionType,
+                                        Some(typeStack.as(result.transformTypes(solution.substitute).expression))
+                                      ).pure[CompilerIO]
+                                    case None    =>
+                                      evaluateToNormalForm(typeStack.as(typeStack.value.signature)).map(_ -> None)
+                                  }
+      resolvedQualifierParams  <- resolveQualifierParams(resolvedValue.name)
+    } yield TypeCheckedValue(
+      resolvedValue.vfqn,
+      resolvedValue.name.as(
+        QualifiedName.from(resolvedValue.name.value, resolvedQualifierParams.map(solution.substitute))
+      ),
+      signatureType,
+      runtime
+    )
   }
 
   private def resolveQualifierParams(
       name: Sourced[ResolveQualifiedName]
-  ): Seq[ExpressionValue] =
+  ): CompilerIO[Seq[ExpressionValue]] =
     name.value.qualifier match {
       case ResolveQualifier.AbilityImplementation(_, expressions) =>
-        expressions.map { expression =>
+        expressions.traverse { expression =>
           evaluateToNormalForm(
             name.as(OperatorResolvedExpression.fromExpression(MatchDesugaredExpression.fromExpression(expression)))
           )
         }
-      case _                                                      => Seq.empty
+      case _                                                      => Seq.empty.pure[CompilerIO]
     }
 }
